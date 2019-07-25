9fb85373d33abf7e3cead65f2ce03f58bdf50ccf
TorOdinEkle <toroek@student.sv.uio.no>
2019-07-25 06:58:45 +0200

    update

--- Program part 3.R
+++ Program part 3.R
@@ -93,4 +93,138 @@ flips
 
 ## Chapter 21.4 For loops vs. functionals
 
+df <- tibble(
+  a = rnorm(10),
+  b = rnorm(10),
+  c = rnorm(10),
+  d = rnorm(10)
+)
+
+output <- vector("double", length(df))
+for (i in seq_along(df)) {
+  output[[i]] <- mean(df[[i]])
+}
+output
+
+
+col_summary <- function(df, fun) {
+  out <- vector("double", length(df))
+  for (i in seq_along(df)) {
+    out[i] <- fun(df[[i]])
+  }
+  out
+}
+col_summary(df, mean)
+
+X <- matrix(rnorm(15), nrow = 5)
+X
+
+apply(X, 1, mean)
+
+
+col_summary2 <- function(df, fun) {
+  # create an empty vector which will store whether each
+  # column is numeric
+  numeric_cols <- vector("logical", length(df))
+  # test whether each column is numeric
+  for (i in seq_along(df)) {
+    numeric_cols[[i]] <- is.numeric(df[[i]])
+  }
+  # find the indexes of the numeric columns
+  idxs <- which(numeric_cols)
+  # find the number of numeric columns
+  n <- sum(numeric_cols)
+  # create a vector to hold the results
+  out <- vector("double", n)
+  # apply the function only to numeric vectors
+  for (i in seq_along(idxs)) {
+    out[[i]] <- fun(df[[idxs[[i]]]])
+  }
+  # name the vector
+  names(out) <- names(df)[idxs]
+  out
+}
+
+df <- tibble(
+  X1 = c(1, 2, 3),
+  X2 = c("A", "B", "C"),
+  X3 = c(0, -1, 5),
+  X4 = c(TRUE, FALSE, TRUE)
+)
+col_summary2(df, mean)
+
+## 21.5 Map functions
+
+models <- mtcars %>% 
+  split(.$cyl) %>% 
+  map(~lm(mpg ~ wt, data = .))
+
+models %>% 
+  map(summary) %>% 
+  map_dbl(~.$r.squared)
+
+## Enda enklere måte
+models %>% 
+  map(summary) %>% 
+  map_dbl("r.squared")
+
+x1 <- list(
+  c(0.27, 0.37, 0.57, 0.91, 0.20),
+  c(0.90, 0.94, 0.66, 0.63, 0.06), 
+  c(0.21, 0.18, 0.69, 0.38, 0.77)
+)
+x2 <- list(
+  c(0.50, 0.72, 0.99, 0.38, 0.78), 
+  c(0.93, 0.21, 0.65, 0.13, 0.27), 
+  c(0.39, 0.01, 0.38, 0.87, 0.34)
+)
+
+threshold <- function(x, cutoff = 0.8) x[x > cutoff]
+x1 %>% sapply(threshold) %>% str()
+x2 %>% sapply(threshold) %>% str()
+
+
+## Exercises 21.5.3
+map_dbl(mtcars, mean)
+map_chr(nycflights13::flights, typeof)
+
+map_int(iris, function(x) length(unique(x)))
+map_int(iris, ~ length(unique(.))) ## Mer kompakt måte, levert av PURRR
+
+map(c(-10, 0, 10, 100), ~ rnorm(n = 10, mean = .)) 
+
+map_lgl(diamonds, is.factor)
+
+x <- split(mtcars, mtcars$cyl)
+View(x)
+
+map(x, function(df) lm(mpg ~ wt, data = df))
+map(x, ~ lm(mpg ~ wt, data = .))
+## Tredje alternativ
+run_reg <- function(df) {
+  lm(mpg ~ wt, data = df)
+}
+map(x, run_reg)
+
+## 21.6 Dealing with failure
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
